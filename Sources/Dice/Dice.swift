9190f14128a629453f53e728f5003919d570f958
Brad Siegfreid <bsiegfreid@me.com>
2020-08-16 10:26:35 -0600

    Updated comments and tests

--- Sources/Dice/Dice.swift
+++ Sources/Dice/Dice.swift
@@ -1,3 +1,7 @@
+/// Roll dice.
+/// - Parameter rolls: Number of times to roll the dice.
+/// - Parameter d: Number of sides of the dice.
+/// - Returns: Total of all dice rolls.
 public func roll(_ rolls: Int, d: Int) -> Int {
     var result = 0;
     for _ in 1...rolls {
--- Tests/DiceTests/DiceTests.swift
+++ Tests/DiceTests/DiceTests.swift
@@ -2,13 +2,23 @@ import XCTest
 @testable import Dice
 
 final class DiceTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    
+    func test1d6() {
+        // Since we have a random value each test will be dealing with a different answer.
+        let result = roll(1, d:6)
+        XCTAssert(result > 0)
+        XCTAssert(result < 7)
+    }
+    
+    func test3d10() {
+        // Since we have a random value each test will be dealing with a different answer.
+        let result = roll(3, d:10)
+        XCTAssert(result > 2)
+        XCTAssert(result < 31)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("test1d6", test1d6),
+        ("test3d10", test3d10),
     ]
 }
